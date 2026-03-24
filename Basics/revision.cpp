7e7ba5d78778b50359165ad49e7fd9030b910f5f
RenSyntax <ren.syntax@gmail.com>
2026-03-24 22:59:44 +0530

    revision

---
+++ Basics/revision.cpp
@@ -0,0 +1,17 @@
+// we can include all the libraries with:
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  // there are data types like:
+  // int, long, long long,
+  // char, float, double.
+
+  // to get a whole line as a input you have to use a internal function:
+  string hello; // declare a string variable
+  getline(cin,
+          hello); // internal function for the whole input of string/sentence
+  cout << hello;  // cout of the input.
+  return 0;
+}
